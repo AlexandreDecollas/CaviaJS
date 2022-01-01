6caaedf0b3418bff97c4052766d1ddc29c79ff1b
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-01-01 22:53:03 +0100

    a first example of e2e test that goes green

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -2,9 +2,11 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from '../src/app.module';
+import { setTimeout } from 'timers/promises';
 
 describe('AppController (e2e)', () => {
   let app: INestApplication;
+  const roomNumber: number = Math.floor(Math.random() * 200);
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
@@ -19,10 +21,14 @@ describe('AppController (e2e)', () => {
     await app.close();
   });
 
-  it('/ (GET)', () => {
+  it(`should be able to reserve a room when name is bibap aloula and room is ${roomNumber}`, async () => {
+    await request(app.getHttpServer()).get('/register/bibap/aloula');
+    await request(app.getHttpServer()).get('/add-room/' + roomNumber);
+    await setTimeout(500);
+
     return request(app.getHttpServer())
-      .get('/')
+      .get('/book-room/check-availability/' + roomNumber)
       .expect(200)
-      .expect('Hello World!');
+      .expect([]);
   });
 });

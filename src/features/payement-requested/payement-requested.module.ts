74d1885c400030af239624c7e892506ceeadf710
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-01-09 17:04:45 +0100

    payment requested as a command

--- src/features/payement-requested/payement-requested.module.ts
+++ src/features/payement-requested/payement-requested.module.ts
@@ -1,9 +1,28 @@
-import { Module } from '@nestjs/common';
-import { PayementRequestedController } from './controllers/payement-requested.controller';
-import { PayementRequestedService } from './services/payement-requested.service';
+import { Command } from '../../event-modelling-tooling/command/command.decorator';
+import { Get, Param } from '@nestjs/common';
+import { Eventbus } from '../../event-modelling-tooling/eventbus/eventbus.service';
+import { IdGeneratorService } from '../../utils/id-generator/id-generator.service';
+import { PayementRequestedEvent } from '../../model/payement-requested.event';
 
-@Module({
-  controllers: [PayementRequestedController],
-  providers: [PayementRequestedService],
+@Command({
+  entryPoint: 'payement-requested',
 })
-export class PayementRequestedModule {}
+export class PayementRequestedModule {
+  constructor(
+    private readonly eventEmitter: Eventbus,
+    private readonly idGeneratorService: IdGeneratorService,
+  ) {}
+
+  @Get('/:clientName')
+  public async register(@Param('clientName') clientName: string) {
+    const event: PayementRequestedEvent = {
+      data: {
+        id: this.idGeneratorService.generateId(),
+        clientName,
+      },
+      metadata: { streamName: 'guest.request-payement' },
+      type: 'PayementRequestedEvent',
+    };
+    this.eventEmitter.emit(event.metadata.streamName, event);
+  }
+}

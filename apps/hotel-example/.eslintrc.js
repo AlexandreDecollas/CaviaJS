0044c415e641709fba5080f9fc79bdbc115da775
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-02-04 00:17:27 +0100

    adding blacklist import of lib folder from app folder

---
+++ apps/hotel-example/.eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  root: true,
+  env: {
+    node: true,
+    jest: true,
+  },
+  ignorePatterns: ['.eslintrc.js'],
+  rules: {
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-restricted-imports': [
+      'error',
+      {
+        patterns: [
+          {
+            group: ['*../lib/cavia-js*', '*cavia-js/*', '*constants*'],
+            message: 'usage of import1 private modules not allowed.',
+          },
+        ],
+      },
+    ],
+  },
+};

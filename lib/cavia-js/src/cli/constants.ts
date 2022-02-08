074b5cf4b4077b7027585f6a64929cb0fbc00ff9
Alexandre Decollas <alexandre.decollas.pro@gmail.com>
2022-02-08 23:41:32 +0100

    improve command help message

--- lib/cavia-js/src/cli/constants.ts
+++ lib/cavia-js/src/cli/constants.ts
@@ -2,7 +2,10 @@ export const CLI_BAD_OPTION_MESSAGE =
   'No or unknown option is given, do nothing... (-h for help)';
 export const CLI_HELP_MESSAGE = `\n\n-==[  CaviaJS Command Line Tool  ]==-\n
 The CLI commands you can use are : 
-      -h  : help
-      -l  : list all the commands imported in the system
-      -ce : returns true if the command has a cli entry point method (a method with @CLI() decorator)
-      -c  : execute the command throw its entry point\n`;
+yarn cli -- -- [-hl] [-c command] [-p argument] 
+      -h          : help
+      -l          : list all the commands imported in the system
+      -ce         : returns true if the command has a cli entry point method (a method with @CLI() decorator)
+      -c command  : execute the command throw its entry point
+      -p arg      : call the command with this sequence of arguments
+`;
